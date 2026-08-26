9c0985cf5feb08c439c2520322337e08aa96c6ec
bhavanaramya <bhavanaramyag@gmail.com>
2026-08-26 18:25:31 +0530

    Add character input and ASCII value display

--- main.c
+++ main.c
@@ -1,1 +1,9 @@
-
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter a character=");
+    scanf("%c", &ch);
+    printf("character=%c\n",ch);
+    printf("ASCII value(decimal)=%d\n",ch);
+}
